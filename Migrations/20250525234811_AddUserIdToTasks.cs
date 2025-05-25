e0a89eaa24a221a7f2596aa100aa97746ec89013
muditjha20 <muditjha1404@gmail.com>
2025-05-25 19:48:22 -0400

    Add UserId to Tasks

---
+++ Migrations/20250525234811_AddUserIdToTasks.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KanbanBoardAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToTasks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Tasks",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Tasks");
+        }
+    }
+}
